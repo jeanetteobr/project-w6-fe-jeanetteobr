12124f49cd0d0f93f0de78a774f5ebe708704202
jeanetteobr <jeanette.obr@gmail.com>
2018-09-07 11:32:07 -0400

    Book titles render on page

--- freeshelf-app/src/App.js
+++ freeshelf-app/src/App.js
@@ -3,12 +3,21 @@ import books from './books.json'
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.books = books
+  }
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>Freeshelf</h1>
         </header>
+        <div className='book-container'>{this.books.map((b, idx) => {
+          return (
+            <div className='book-title' key={idx}><h1>{b.title}</h1></div>
+          )
+        })}</div>
       </div>
     )
   }

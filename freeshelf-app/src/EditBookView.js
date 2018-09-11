ed40617393c96931a2d041f3aeaa41f307ccd7f7
jeanetteobr <jeanette.obr@gmail.com>
2018-09-11 09:18:30 -0400

    fixed bug in detailed description text area

--- freeshelf-app/src/EditBookView.js
+++ freeshelf-app/src/EditBookView.js
@@ -22,13 +22,13 @@ class EditBookView extends Component {
           <label for='short-desc'>Short description:</label>
           <textarea className='input' id='short-desc' type='text' form='edit-book' value={books.shortDescription} onChange={(e) => { this.props.updateBook(books.id, 'short desc', e.target.value) }} />
           <label for='cover-img'>Cover image url:</label>
-          <input className='input' id='cover-img' type='text' value={books.coverImageUrl} onChange={(e) => { this.props.updateBook(books.id, 'cover url', e.target.value) }} />
+          <input className='input' id='cover-img' type='text' value={books.coverImageUrl} onChange={(e) => { this.props.updateBook(books.id, 'cover img', e.target.value) }} />
           <label for='publisher'>Publisher:</label>
           <input className='input' id='publisher' type='text' value={books.publisher} onChange={(e) => { this.props.updateBook(books.id, 'publisher', e.target.value) }} />
           <label for='publish-date'>Publication date:</label>
-          <input className='input' id='pubish-date' type='text' value={books.publicationDate} onChange={(e) => { this.props.updateBook(books.id, 'publish date', e.target.value) }} />
+          <input className='input' id='date' type='text' value={books.publicationDate} onChange={(e) => { this.props.updateBook(books.id, 'date', e.target.value) }} />
           <label for='detailed-desc'>Detailed description:</label>
-          <textarea className='textarea' id='detailed-desc' form='edit-book' type='text' rows='6' value={books.detailedDescription} />
+          <textarea className='textarea' id='detailed-desc' form='edit-book' type='text' rows='6' value={books.detailedDescription} onChange={(e) => { this.props.updateBook(books.id, 'detailed3', e.target.value) }} />
           <input className='button is-small is-primary' type='submit' value='Submit' />
         </form>
         <div><button className='button is-small is-primary' onClick={() => editBook(null)}>Return to book list</button></div>
